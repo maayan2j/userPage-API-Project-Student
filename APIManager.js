d5d960bac238233b111dfff50678c584a063e9dc
maayan2j <maayan2j@gmail.com>
2020-04-27 16:44:37 +0300

    main

--- APIManager.js
+++ APIManager.js
@@ -35,3 +35,16 @@ class APIManager {
 
 
 
+//         $.ajax
+//         let userData = $.get(`https://randomuser.me/api/`);
+//         userData.done(function(data){
+//         //let users = []
+//         //let mainUser = [picture, a first name & last name, the city and state.]
+//         //let friendsUsers = [first: last:]
+
+//         // for (let user of users){
+
+            
+//         //      // push.data.results[0-6]
+
+//         // }
--- main.js
+++ main.js
@@ -7,45 +7,12 @@ const apiManager = new apiManager()
 // Clicking on Load button.
 
 $('#load').on("click", function() {
-            
-    
-            console.log("Load button was clicked.")
-            $.ajax
-            let userData = $.get(`https://randomuser.me/api/`);
-            userData.done(function(data){
-            //let users = []
-            //let mainUser = [picture, a first name & last name, the city and state.]
-            //let friendsUsers = [first: last:]
+console.log("Load button was clicked.")
 
-            // for (let user of users){
-
-                
-            //      // push.data.results[0-6]
-
-            // }
-                    
-    
-            })
-
+    apiManager.getMainUser()
+    apiManager.getFriends()
+    apiManager.getQuote()
 })
-
-
-
-
-// Quote generator:
-
-// $('#load').on("click", function() {
-//             
-//             $.ajax
-//             let kaneyQuote = $.get(`https://api.kanye.rest`);
-//             kaneyQuote.done(function(){
-
-    
-//             })
-//     return kaneyQuote;
-// })
-
-
 
 
 
@@ -53,30 +20,17 @@ $('#load').on("click", function() {
 // Clicking on Display button.
 
 $('#load').on("click", function() {
+console.log("Display button was clicked.")
 
-    console.log("Display button was clicked.")
+    const mainuser = apiManager.data.quote
+    render.renderQuote(mainuser)
+    const friends = apiManager.data.quote
+    render.renderQuote(friends)
+    const quote = apiManager.data.quote
+    render.renderQuote(quote)
+
 })
 
 
 
 
-
-
-
-
-// API Manager - Users data, Kanye quote, Pokemon and about.
-
-let apiManager= function(){
-
-    $('#load').on("click", function() {
-            
-                $.ajax
-                let userData = $.get(`https://randomuser.me/api/`);    
-                let kaneyQuote = $.get(`https://api.kanye.rest`);
-
-    })
-
-    //return users, Kanye quote
-    //Pokemon and about
-
-}
