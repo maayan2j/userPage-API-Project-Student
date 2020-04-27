19c9e22e884ed1a4bf1fed8921927792fe5b4489
maayan2j <maayan2j@gmail.com>
2020-04-27 15:28:28 +0300

    Render

--- Renderer.js
+++ Renderer.js
@@ -1,4 +1,40 @@
 
 class Renderer {
 
+    constructor(){
+        //Main User 
+        this.mainUserSource = $(`quote-template`).html()
+        this.mainUserTemplate = Handlebars.compile(this.mainUserSource)
+        //Friends 
+        this.friendsSource = $(`quote-template`).html()
+        this.friendsTemplate = Handlebars.compile(this.friendsSource)
+        //Kanye Quote
+        this.quoteSource = $(`quote-template`).html()
+        this.quoteTemplate = Handlebars.compile(this.qouteSource)
+        //Pokemon 
+        // this.pokemonSource = $(`pokemon-template`).html()
+        // this.sourceTemplate = Handlebars.compile(this.pokemonSource)
+
+    }
+    
+    renderQuote(mainUser){
+        const newQuote = this.sourceTemplate({quote: kanyeQuote})
+        $(`.quote-container`).append(newQuote)
+    }
+
+    
+    renderQuote(kanyeQuote){
+        const newQuote = this.sourceTemplate({quote: kanyeQuote})
+        $(`.quote-container`).append(newQuote)
+    }
+
+
+    renderQuote(kanyeQuote){
+        const newQuote = this.squoteTemplate({quote: kanyeQuote})
+        $(`.quote-container`).append(newQuote)
+    }
+
+
+
+
 }
---
+++ main.js
@@ -0,0 +1,83 @@
+
+
+
+
+
+
+
+// Clicking on Load button.
+
+$('#load').on("click", function() {
+            
+    
+            console.log("Load button was clicked.")
+            $.ajax
+            let userData = $.get(`https://randomuser.me/api/`);
+            userData.done(function(data){
+            //let users = []
+            //let mainUser = [picture, a first name & last name, the city and state.]
+            //let friendsUsers = [first: last:]
+
+            // for (let user of users){
+
+                
+            //      // push.data.results[0-6]
+
+            // }
+                    
+    
+            })
+
+})
+
+
+
+
+// Quote generator:
+
+// $('#load').on("click", function() {
+//             
+//             $.ajax
+//             let kaneyQuote = $.get(`https://api.kanye.rest`);
+//             kaneyQuote.done(function(){
+
+    
+//             })
+//     return kaneyQuote;
+// })
+
+
+
+
+
+
+// Clicking on Display button.
+
+$('#load').on("click", function() {
+
+    console.log("Display button was clicked.")
+})
+
+
+
+
+
+
+
+
+// API Manager - Users data, Kanye quote, Pokemon and about.
+
+let apiManager= function(){
+
+    $('#load').on("click", function() {
+            
+                $.ajax
+                let userData = $.get(`https://randomuser.me/api/`);    
+                let kaneyQuote = $.get(`https://api.kanye.rest`);
+
+    })
+
+    //return users, Kanye quote
+    //Pokemon and about
+
+}
